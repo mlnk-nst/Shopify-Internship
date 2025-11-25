d4dc8ae8b5511abb8d5feb8de77ae72d585c0e2e
Anastasiіa <imperatreale@gmail.com>
2025-11-25 14:11:09 +0200

    Shopify Internship- EI-3: updated mobile adaptation for button

--- assets/shop-now.js
+++ assets/shop-now.js
@@ -7,17 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         const productCard = this.closest('.image-container');
         const shopNowBtn = productCard.querySelector('.shop-now-button');
-        
         shopNowBtn.classList.toggle('show');
-      }
-    });
-  });
-  
-  const mobileIcons = document.querySelectorAll('.icon-container');
-  mobileIcons.forEach(icon => {
-    icon.addEventListener('click', function(e) {
-      if (window.innerWidth <= 768) {
-        const productUrl = this.closest('.image-container').querySelector('a').href;
+      } else {
+        e.preventDefault();
+        const productUrl = this.closest('.image-container').querySelector('a[href]').href;
         window.location.href = productUrl;
       }
     });
